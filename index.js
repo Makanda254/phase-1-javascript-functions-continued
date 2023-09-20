0fa4329950636a0b2ef6048ae114f8ec496bc49a
Victor John Makanda <victormakanda254@gmail.com>
2023-09-21 00:37:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork= function(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(operator = "*") {
+    return function ( adjective = "special") {
+      return `You are ${operator}${adjective}${operator}!`;
+    };
+  }
+
+  wrapAdjective("%")("not special");
